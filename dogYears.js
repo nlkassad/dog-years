2026d83cf58862481e3d7a7975dfd9798105ae8f
nlkassad <nlkassad@hotmail.com>
2016-12-07 13:42:42 -0500

    Add dog years calculation
    -add the function to calculate dog years from the code academy practice

---
+++ dogYears.js
@@ -0,0 +1,10 @@
+// get user age
+var myAge = 36;
+// create value for first two years
+var firstYears = (10.5 * 2);
+// calculate age in dog years less first two years
+var laterYears = ((myAge - 2) * 4);
+// add value for first two years and value for rest of years
+var myAgeInDogYears = (firstYears + laterYears);
+// log dog years age
+console.log(myAgeInDogYears);
